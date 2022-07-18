59c05b12cf3e44e0ed23578f81734ac45226a1cf
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-07-18 20:04:36 -0300

    update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,29 +1,33 @@
-import './style.css';
+import "./style.css";
 
 const Header = () => {
   return (
-<header className="header">
-    <section className="flex">
+    <header className="header">
+
+      <section className="flex">
         <div className="nome-perfil">Ezio Auditore</div>
-        <a className="foto-perfil" href="/" title='Foto de perfil'></a>
-    </section>
-    <nav className='flex-container'>
+        <a className="foto-perfil" href="/" title="Foto de perfil"></a>
+      </section>
+
+      <nav className="flex-container">
         <ul className="menu">
 
-            <div className="button">
-                <li className="btn-icone"/>
-                <li className="btn-texto">VOLTAR</li>
-            </div>
+          <div className="button">
+            <li className="btn-icone" />
+            <li className="btn-texto">VOLTAR</li>
+          </div>
 
-            <li  className="btn-addgame" />
+          <li className="btn-addgame" />
 
-            <li className="time-hour">
-                <a> 06/07/2022 16:09</a>
-            </li>
+          <li className="time-hour">
+            <a> 06/07/2022 16:09</a>
+          </li>
+          
         </ul>
-    </nav>
-  </header>
-  )
-}
+      </nav>
 
-export default Header
+    </header>
+  );
+};
+
+export default Header;
